9c201fd87f77189c6665e2d4ebdf72bf73290d54
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-12-03 22:29:36 -0300

    Adicionar arquivos de projeto.

---
+++ LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int cont = 1;
+
+            Console.WriteLine("Digite um número maior que 0: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero > 0)
+            {
+                while(cont <= numero)
+                {
+                    Console.WriteLine(cont);
+                    cont++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Digite apenas números maiores que zero! ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
